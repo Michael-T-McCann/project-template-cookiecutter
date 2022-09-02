a65ad21986226da6c0202f9f22bab387156c6572
Michael McCann <mccann@.lanl.gov>
2022-09-02 09:14:21 -0600

    Use package name

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,7 +5,7 @@ Initialize the repo and commit
 
 cd {{cookiecutter.repo_name}}
 git init --initial-branch main
-git add .gitignore README.md data/README.md  docs/README.md environment.yml results/README.md scripts/README.md setup.py src/__init__.py tests/README.md
+git add .gitignore README.md data/README.md  docs/README.md environment.yml results/README.md scripts/README.md setup.py {{package_name}}/__init__.py tests/README.md
 git commit -m"Add files from template"
 
 (Optional) Make repo on github and push to there.
